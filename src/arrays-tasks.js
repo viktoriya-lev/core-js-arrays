21ceaa0b2bb6392c1d02dd4587e8f0582a7459b9
Viktoriya Levitskaya <toriya.levitskaya@mail.ru>
2025-01-12 00:09:39 +0300

    feat: complete one more task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+
+  return Array.from(
+    { length: maxLength },
+    (item, index) => (arr1[index] || 0) + (arr2[index] || 0)
+  );
 }
 
 /**
